e5d193122af85f35de5e7f9d36a266f852245a38
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2026-09-07 20:02:59 +0530

    autoscaling added

--- main.tf
+++ main.tf
@@ -97,3 +97,26 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.web.id
   port             = 80
 }
+
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.3.1"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.vpc.public_subnets
+
+  launch_template_name = "blog"
+  security_groups       = [module.blog_sg.security_group_id]
+  instance_type          = var.instance_type
+  image_id               = data.aws_ami.app_ami.id
+
+  traffic_source_attachments = {
+    blog_alb = {
+      traffic_source_identifier = aws_lb_target_group.test.arn
+    }
+  }
+}
