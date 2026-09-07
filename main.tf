b84bd8ed0f547b103d92ab28a33e84935241a49b
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2026-09-07 13:07:07 +0530

    Add AWS instance resource configuration
    added the resource block for aws instance creation

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ data "aws_ami" "app_ami" {
 
   owners = ["amazon"]
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
