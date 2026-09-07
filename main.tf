9e8e551f17df60e85a414dab3bb9a65819d1456b
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2026-09-07 19:17:44 +0530

    Add VPC module and place web instance in public subnet with security group

--- main.tf
+++ main.tf
@@ -14,14 +14,30 @@ data "aws_ami" "app_ami" {
   owners = ["amazon"]
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform hands-on"
@@ -33,44 +49,11 @@ module "blog_sg" {
   version = "4.13.0"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "allow inbound traffic through http and https. Allow everything out"
-  vpc_id      = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_inbound" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_inbound" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_outbound" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
 }
